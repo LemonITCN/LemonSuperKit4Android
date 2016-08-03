9dc6376dddd7c2bcfac3259f65200d5571da0c2e
LiuRi <liuri@lemonsoft.net>
2016-08-03 14:33:06 +0800

    初始化构建工程

---
+++ LemonKit4Android/app/src/main/java/net/lemonsoft/lemonkit4android/ui/core/LKUIWindow.java
@@ -0,0 +1,12 @@
+package net.lemonsoft.lemonkit4android.ui.core;
+
+import android.app.Activity;
+
+/**
+ * Created by LiuRi on 16/8/3.
+ */
+public class LKUIWindow extends Activity {
+
+
+
+}
---
+++ LemonKit4Android/app/src/main/java/net/lemonsoft/lemonkit4android/ui/view/LKUIToolBar.java
@@ -0,0 +1,7 @@
+package net.lemonsoft.lemonkit4android.ui.view;
+
+/**
+ * Created by LiuRi on 16/8/3.
+ */
+public class LKUIToolBar {
+}
