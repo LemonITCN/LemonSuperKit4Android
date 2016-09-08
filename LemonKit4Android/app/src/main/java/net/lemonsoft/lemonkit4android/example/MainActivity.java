e62fcb2987d8573508124983c3c93a5698d875e4
LiuRi <liuri@lemonsoft.net>
2016-09-08 11:37:45 +0800

    完成了LKNotification的开发

--- LemonKit4Android/app/src/main/java/net/lemonsoft/lemonkit4android/example/MainActivity.java
+++ LemonKit4Android/app/src/main/java/net/lemonsoft/lemonkit4android/example/MainActivity.java
@@ -1,9 +1,14 @@
 package net.lemonsoft.lemonkit4android.example;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import net.lemonsoft.lemonkit4android.R;
 import net.lemonsoft.lemonkit4android.ui.core.LKUIWindow;
+import net.lemonsoft.lemonkit4android.ui.tip.LKNotification.LKNotification;
 
 public class MainActivity extends LKUIWindow {
 
@@ -11,5 +16,19 @@ public class MainActivity extends LKUIWindow {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final Button button = (Button) findViewById(R.id.showLKNotification);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                LKNotification.setDefault_icon(getApplicationContext(), R.mipmap.icon);
+                LKNotification.show(MainActivity.this, "这是一个LKNotification", "这是一个LKNotification的通知。正在进行测试显示。这个控件还有对应的iOS版本哦！");
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
     }
 }
