80c234a2bf2c2c648d77047493692d3467e19dc1
LiuRi <liuri@lemonsoft.net>
2016-12-25 19:58:44 +0800

    renew LemonKit 4 android

---
+++ LemonKit/lemonkit-samples/src/main/java/net/lemonsoft/lemonkit/samples/MainActivity.java
@@ -0,0 +1,13 @@
+package net.lemonsoft.lemonkit.samples;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ LemonKit/lemonkit/src/main/java/net/lemonsoft/lemonkit/LemonKit.java
@@ -0,0 +1,8 @@
+package net.lemonsoft.lemonkit;
+
+/**
+ * Created by LiuRi on 2016/12/24.
+ */
+
+public class LemonKit {
+}
