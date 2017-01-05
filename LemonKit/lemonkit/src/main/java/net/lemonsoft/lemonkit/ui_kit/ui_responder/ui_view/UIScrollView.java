b6ccb25cd8ca3a9fae5e674bc72662502d6a5af0
LiuRi <liuri@lemonsoft.net>
2017-01-05 17:02:04 +0800

    添加UIScrollView

---
+++ LemonKit/lemonkit/src/main/java/net/lemonsoft/lemonkit/ui_kit/ui_responder/ui_view/UIScrollView.java
@@ -0,0 +1,10 @@
+package net.lemonsoft.lemonkit.ui_kit.ui_responder.ui_view;
+
+/**
+ * UIScrollView 对 LKScrollView的封装
+ * 让其和iOS的使用更相近，所有的单位都换成DP
+ * Created by LiuRi on 2017/1/5.
+ */
+
+public class UIScrollView<LKScrollView> extends UIView {
+}
